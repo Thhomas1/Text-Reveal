14adf2904f49d47296f6f953e8f6529845cf6b30
Thhomas1 <thomasromero10@hotmail.com>
2023-01-16 17:29:35 -0300

    More split animations

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ tl.fromTo('.cta4', {x: '100%', opacity: 0.5}, {x:0, opacity: 1}, "<20%");
 tl.fromTo('.cta6', {x: '-100%', opacity: 0.5}, {x:0, opacity: 1}, "<20%");
 tl.fromTo('.cta5', {y: '100%', opacity: 0.5}, {y:0, opacity: 1}, "<20%");  
 tl.fromTo('.cta-btn', {y: 20, opacity: 0}, {y:0, opacity: 1}, "<");  
+
+// Text alternative
+
+const logo = document.querySelector('.logo');
+const letters = logo.textContent.split("");   // al utilizar el split podemos ver que las palabras del logo "S K I N C A R E" se juntan para declararlas en una constante
+
+letters.forEach( (letter) => {
+    logo.innerHTML +='<span class="letter">' + letter + "</span>";
+});
+
+
